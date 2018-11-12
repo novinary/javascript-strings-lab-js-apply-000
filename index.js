ad761929468d926984501129319a96800e340c6a
captivating-computer-3358 <novinary@hotmail.com>
2018-11-12 21:40:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var specialGuest = "Neil deGrasse Tyson"
 var greetSpecialGuest = "Hello," + " " + specialGuest + "!";
 
 var topic = "Let's talk about space" + " " + "Let's talk about space" + " " + "Let's talk about space" + " " + "Let's talk about space"
-+ " " + "Let's talk about space" + "Let's talk about space....";
++ " " + "Let's talk about space" + " " + "Let's talk about space" + "Let's talk about space.....";
 
 var conversation = `${topic}`;
