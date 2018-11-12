268285dbaddd636f8793583e557888cec288515b
captivating-computer-3358 <novinary@hotmail.com>
2018-11-12 21:43:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello," + " " + specialGuest + "!";
 
-var topic = "Let's talk about" + " " + "Let's talk about space" + " " + "Let's talk about space" + " " + "Let's talk about space"
-+ " " + "Let's talk about space" + " " + "Let's talk about space"+ " " + "Let's talk about space" + "Let's talk about space......";
+//var topic = "Let's talk about" + " " + "Let's talk about space" + " " + "Let's talk about space" + " " + "Let's talk about space"
+//+ " " + "Let's talk about space" + " " + "Let's talk about space"+ " " + "Let's talk about space" + "Let's talk about space......";
+var topic = "space";
 
 var conversation = `${topic}`;
